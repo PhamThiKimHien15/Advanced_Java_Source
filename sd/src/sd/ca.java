e6b6f82eeaad0bbe7475f7c2e66562d5d776f0c5
PhamThiKimHien15 <phamthikimhien2991@gmail.com>
2016-08-18 17:24:31 +0700

    Chapter1Part1PhamThiKimHien

--- sd/src/sd/ca.java
+++
@@ -1,8 +0,0 @@
-package sd;
-
-public class ca {
-
-	public static void main(String[] args) {
-		System.out.println("ds");
-	}
-}
--- sd/src/sd/sss.java
+++
@@ -1,7 +0,0 @@
-package sd;
-
-public class sss {
-public static void main(String[] args) {
-	System.out.println("nds");
-}
-}
